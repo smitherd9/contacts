4469c32da830b0b10aa353219a879cab84553651
Daniel Smither <smitherd9@gmail.com>
2016-09-05 09:39:17 -0400

    working with objects practice

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-
 var Contact = {
-	firstName: "",
-	lastName: "",
-	phoneNumber: "",
-	street: "",
-	city: "",
-	state: ""
+    firstName: "",
+    lastName: "",
+    phoneNumber: "",
+    street: "",
+    city: "",
+    state: ""
 };
 
 var submissionArray = [];
@@ -13,35 +12,50 @@ var submissionArray = [];
 $(document).ready(function() {
 
 
-$('#add').click(function(e){
-	e.preventDefault();
-	var submission = Object.create(Contact);
-	submission.firstName = $('#first-name').val();
-	submission.lastName = $('#last-name').val();
-	submission.phoneNumber = $('#phone-number').val();
-	submission.street = $('#street').val();
-	submission.city = $('#city').val();
-	submission.state = $('#state').val();
-	submissionArray.push(submission);
-	$('#contact').append('<li>' + submission.firstName + '</li>');
-});
+    $('#add').click(function(e) {
+        e.preventDefault();
+        var submission = Object.create(Contact);
+        submission.firstName = $('#first-name').val();
+        submission.lastName = $('#last-name').val();
+        submission.phoneNumber = $('#phone-number').val();
+        submission.street = $('#street').val();
+        submission.city = $('#city').val();
+        submission.state = $('#state').val();
+        submissionArray.push(submission);
+        // $('#contact').append('<li>' + submission.firstName + '</li>');
+        console.log(submissionArray);
 
-$('#contact').click(function(){
-	// alert('wassup yo');
-	var index = $('#contact').index(this);
 
-	$('.show-contact').data('test', Contact)
-	$('.show-contact').append(Contact);
-});
+        //DOM manipulation
+        var elem = $('<div>');
+        elem.text(submission.firstName);
+        elem.addClass('contact');
+        elem.data('index', submissionArray.length - 1);  // array location starts at 0 but the length of an array starts at 1
+                                                         // if there is one item in an array, to find the index location you must subtract 1
+        $('.show-contact').append(elem);
 
-// $('.form-group').submit(function(e){
-// 	e.preventDefault();
-// 	var input = $('.form-control').val();
-// 	$('.form-control').val("");
 
-// });
+    });
 
-// var submission = new Contact;
+    $('.show-contact').click(function() {
+        // alert('wassup yo');
+        var i = $(submissionArray[i]).data('index');
+        console.log(i);
+
+        var c = submissionArray[i];
+        console.log(c);
+
+        $(this).append(' <span class="contactInfo">' + c + '</span>');
+    });
+
+    // $('.form-group').submit(function(e){
+    // 	e.preventDefault();
+    // 	var input = $('.form-control').val();
+    // 	$('.form-control').val("");
+
+    // });
+
+    // var submission = new Contact;
 
 
 
